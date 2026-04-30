7fe4415eae434681d1a23d88bca15fc689fa0cef
mgrahul63 <mgrahul639@gmail.com>
2026-04-30 08:04:27 +0600

    upload new vocab

--- data/level0/index.js
+++ data/level0/index.js
@@ -9,10 +9,34 @@ const day8 = require("./day8.json");
 const day9 = require("./day9.json");
 const day10 = require("./day10.json");
 const day11 = require("./day11.json");
+const day12 = require("./day12.json");
+const day13 = require("./day13.json");
+const day14 = require("./day14.json");
+const day15 = require("./day15.json");
+const day16 = require("./day16.json");
+const day17 = require("./day17.json");
 
 module.exports = {
   levelId: 0,
   levelName: "Beginner",
   emoji: "🌱",
-  days: [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11],
+  days: [
+    day1,
+    day2,
+    day3,
+    day4,
+    day5,
+    day6,
+    day7,
+    day8,
+    day9,
+    day10,
+    day11,
+    day12,
+    day13,
+    day14,
+    day15,
+    day16,
+    day17,
+  ],
 };
