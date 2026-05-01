575395a6fdc8608cca9a6b02f425f7d321343e48
mgrahul63 <mgrahul639@gmail.com>
2026-05-01 23:29:02 +0600

    add more word

--- data/words/index.js
+++ data/words/index.js
@@ -7,23 +7,23 @@ const wordsF = require("./wordsF.json");
 const wordsG = require("./wordsG.json");
 const wordsH = require("./wordsH.json");
 const wordsI = require("./wordsI.json");
-// const wordsJ = require("./wordsJ.json");
-// const wordsK = require("./wordsK.json");
-// const wordsL = require("./wordsL.json");
+const wordsJ = require("./wordsJ.json");
+const wordsK = require("./wordsK.json");
+const wordsL = require("./wordsL.json");
 const wordsM = require("./wordsM.json");
-// const wordsN = require("./wordsN.json");
-// const wordsO = require("./wordsO.json");
+const wordsN = require("./wordsN.json");
+const wordsO = require("./wordsO.json");
 const wordsP = require("./wordsP.json");
-// const wordsQ = require("./wordsQ.json");
-// const wordsR = require("./wordsR.json");
+const wordsQ = require("./wordsQ.json");
+const wordsR = require("./wordsR.json");
 const wordsS = require("./wordsS.json");
 const wordsT = require("./wordsT.json");
-// const wordsU = require("./wordsU.json");
-// const wordsV = require("./wordsV.json");
+const wordsU = require("./wordsU.json");
+const wordsV = require("./wordsV.json");
 const wordsW = require("./wordsW.json");
-// const wordsX = require("./wordsX.json");
-// const wordsY = require("./wordsY.json");
-// const wordsZ = require("./wordsZ.json");
+const wordsX = require("./wordsX.json");
+const wordsY = require("./wordsY.json");
+const wordsZ = require("./wordsZ.json");
 
 module.exports = {
   words: [
@@ -36,10 +36,22 @@ module.exports = {
     ...wordsG,
     ...wordsH,
     ...wordsI,
+    ...wordsJ,
+    ...wordsK,
+    ...wordsL,
     ...wordsM,
+    ...wordsN,
+    ...wordsO,
     ...wordsP,
+    ...wordsQ,
+    ...wordsR,
     ...wordsS,
     ...wordsT,
+    ...wordsU,
+    ...wordsV,
     ...wordsW,
+    ...wordsX,
+    ...wordsY,
+    ...wordsZ,
   ],
 };
